f659b830b467b33669d32c74e4a9041d7f1b318e
zhangzju <zhangway19921221@163.com>
2016-12-12 02:24:57 +0800

    add basic mock data

--- src/GetAxios.vue
+++ src/GetAxios.vue
@@ -1,6 +1,8 @@
 <template>
   <div id="app">
-
+    <p>
+      {{ msg }}
+    </p>
   </div>
 </template>
 
@@ -11,7 +13,14 @@ export default {
     return {
       msg: 'Welcome to Your Vue.js App'
     }
-  }
+  },
+  created () {
+    // this.axios.get(http://op.juhe.cn/onebox/weather/query?cityname=%E6%B7%B1%E5%9C%B3&dtype=json&key=9ff894565b4c05721ccac60d907eb26c).then((response) => {
+    //   this.msg = response.data.result;
+    //   console.log(response.data);
+    // })
+    this.msg = 'Changed by Created lifycircle!'
+   }
 }
 </script>
 
--- src/main.js
+++ src/main.js
@@ -1,7 +1,12 @@
 import Vue from 'vue'
-import App from './App.vue'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
+
+import GetAxios from './GetAxios.vue'
+
+Vue.use(VueAxios, axios)
 
 new Vue({
   el: '#app',
-  render: h => h(App)
+  render: h => h(GetAxios)
 })
